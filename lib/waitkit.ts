020fc5eb10ac2e0858ff429f8b31f29b0fabb8eb
Ubeyid Oumer <159727076+ubeyidah@users.noreply.github.com>
2026-06-13 20:58:50 +0300

    Update waitkit.ts

--- lib/waitkit.ts
+++ lib/waitkit.ts
@@ -3,5 +3,5 @@ import { WaitKit } from "waitkit"
 export const wk = new WaitKit({
   apiKey: process.env.WAITKIT_API_KEY || "",
   projectSlug: process.env.WAITKIT_PROJECT_SLUG || "",
-  baseUrl: "https://waitkit-863c1ab981a9.herokuapp.com",
+  baseUrl: "https://waitkit-5494a8d58dca.herokuapp.com",
 })
